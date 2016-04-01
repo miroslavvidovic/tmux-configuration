141cc76d8cbeabc92364e6ac1430ed31ef6c653b
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-04-01 12:07:40 +0200

    Check if tmux and tmuxinator are installed

--- setup_tmux.sh
+++ setup_tmux.sh
@@ -14,10 +14,30 @@
 # -------------------------------------------------------
 # Script:
 
+# Check for tmuxinator
+# If a program  is installed return 0
+# and if not return 1
+# Function takes the program name as the first param
+is_installed() {
+    if ! type "$1" 2> /dev/null; then
+        return 1
+    else
+        return 0
+    fi
+}
+
 # Copy tmux conf
-cp -i tmux.conf ~/.tmux.conf
+if is_installed tmux ; then
+  cp -i tmux.conf ~/.tmux.conf
+else
+  echo "Tmux not installed. Install tmux first."
+fi
 
 # Copy all tmuxinator files
-cp tmuxinator/* ~/.tmuxinator
+if is_installed tmuxinator ; then
+  cp tmuxinator/* ~/.tmuxinator
+else
+  echo "Tmuxinator not installed. Install tmuxinator first."
+fi
 
 exit 0
